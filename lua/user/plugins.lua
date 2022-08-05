26d02c58dc1970d94fd297eb547734207d1b040a
JimKarpodinis <dkarpodinis2@gmail.com>
2022-08-05 07:56:18 +0300

    change colorscheme to nightfox

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -48,7 +48,7 @@ return packer.startup(function(use)
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
-
+  use "EdenEast/nightfox.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
