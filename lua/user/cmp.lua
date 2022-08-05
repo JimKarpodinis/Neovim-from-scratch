17849f237a0dc1e23d76745a8f2ce4ab191adc63
JimKarpodinis <dkarpodinis2@gmail.com>
2022-08-05 11:41:05 +0300

    change telescope colorscheme and debug

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -119,11 +119,14 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
+  window = {
   documentation = {
     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
   },
+},
   experimental = {
     ghost_text = false,
     native_menu = false,
   },
 }
+
