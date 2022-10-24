b50825a9218caae6cf0dc676a9317f3ffbdb67b6
JimKarpodinis <dkarpodinis2@gmail.com>
2022-10-24 14:06:48 +0300

    chore: change colorscheme to nightfox

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,9 @@
-local colorscheme = "nightfox"
+vim.cmd [[
+try
+  colorscheme nightfox
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  return
-end
